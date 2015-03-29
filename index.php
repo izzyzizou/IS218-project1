61ab730ea76174f16ced14aaa45a6b0eb9528672
izzat abdujabbarov <ia85@afsconnect2.njit.edu>
2015-03-29 15:51:28 -0400

    Update1

--- index.php
+++ index.php
@@ -1,1 +1,20 @@
-<?php echo 'hello'; ?>
+<?php
+echo "<html><body><table>\n\n";
+$csv = fopen("hd2013.csv", "r");
+	while(($line = fgetcsv($csv)) !== FALSE);
+	{
+	$count = 0;
+	echo "<tr>";
+	foreach($line as $cell)
+	{
+		if($count<3)
+		{
+			echo "<td>" . htmlspecialchars($cell) . "</td>";
+			$count++;
+		}
+	}
+	echo "<tr>\n";
+}
+fclose($csv);
+echo "\n</table></body></html>";
+?>
